0151129adfaabeba0701395945d6020b118949b4
Nik Nyby <nikolas@gnu.org>
2017-11-28 00:18:40 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import praw
+import re
+import os
+import time
+
+
+def main():
+    reddit = praw.Reddit('NoMobileArticlesBot')
+
+    if not os.path.isfile('posts_replied_to.txt'):
+        posts_replied_to = []
+
+    else:
+        # Read the file into a list and remove any empty values
+        with open('posts_replied_to.txt', 'r') as f:
+            posts_replied_to = f.read()
+            posts_replied_to = posts_replied_to.split('\n')
+            posts_replied_to = list(filter(None, posts_replied_to))
+
+    subreddit = reddit.subreddit('wikipedia')
+    for submission in subreddit.hot(limit=10):
+        print(submission.title)
+        print(submission.url)
+
+        if submission.url.startswith('https://en.m.wikipedia.org/') and \
+           (submission.id not in posts_replied_to):
+            slug = submission.url.split('/')[-1]
+            main_url = 'https://en.wikipedia.org/wiki/{}'.format(slug)
+            submission.reply(
+                'Hi. You linked to the mobile version of this ' +
+                'article. The main one is at: ' + main_url)
+            print('Bot replying to : ', submission.title)
+
+            posts_replied_to.append(submission.id)
+            time.sleep(4)
+
+    # Write our updated list back to the file
+    with open('posts_replied_to.txt', 'w') as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + '\n')
+
+if __name__ == '__main__':
+    main();
